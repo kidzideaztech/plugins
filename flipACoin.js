d31d445a030c1e88cd306250fd30f64081777fdf
EC2 Default User <ec2-user@ip-172-31-30-187.us-east-2.compute.internal>
2018-02-11 00:30:39 +0000

    Add Stickers

---
+++ flipACoin.js
@@ -0,0 +1,19 @@
+if(Meteor.isServer)
+{
+    
+    _PluginFlipACoin = function (chatId) {
+        
+        if(Math.random() >= 0.5)
+        {
+            coin = "Tails";
+        }
+        else
+        {
+            coin = "Heads";
+        }
+        
+        PluginAPI.sendChatMessage(chatId, "<br><small>Flipped a coin. It landed:</small><h1 style='margin: 0px;'>" + coin + "</h1>");
+        
+    }
+    
+}
