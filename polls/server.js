c9dcc470cfe880b4dc5558c779f4ef23a81fc94a
Nathaniel Dsouza <nate@kidzideaz.xyz>
2018-03-05 15:25:26 -0500

    Update server.js

--- polls/server.js
+++ polls/server.js
@@ -1,21 +1,20 @@
 if(Meteor.isServer)
 {
     Meteor.publish("_poll", function (pollId) {
+        check(pollId, String);
         return _PollsCollection.find({_id:pollId});
-    })
+    });
     
     // Clear polls older then 30 days
     now = new Date(),
-    hourAgo = new Date(now.getTime() - (
+    timeToRemove = new Date(now.getTime() - (
         1000 * // Miliseconds
         60 *  // Seconds
         60 * // Minutes
         24 * // Hours
-        30 * // Days
-        1
+        30 // Days
         )
     );
-    toRemove = Messages.find({"time": { "$lte": hourAgo }}).fetch();
-    _PollsCollection.find({"creationDate": { "$lte": hourAgo }}).fetch();
+    _PollsCollection.find({"creationDate": {"$lte":timeToRemove}}).fetch();
     
 }
